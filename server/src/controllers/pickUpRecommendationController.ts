f263d07bae8d95fa7249df975a99be7335d0728d
Maria Padilla <maria.pad.mar@gmail.com>
2025-12-22 12:06:17 +0100

    feat: return a PickUpRecommendation with hardcoded response from possible external api

--- server/src/controllers/pickUpRecommendationController.ts
+++ server/src/controllers/pickUpRecommendationController.ts
@@ -3,9 +3,28 @@ import { buildPickUpRecommendation } from "../services/pickUpRecommendationServi
 
 export const getPickUpRecommendation = async (req: Request, res: Response) => {
   try {
-    const recommendation = await buildPickUpRecommendation();
+    const flightArrivalTime = req.query.arrival;
+    if (!flightArrivalTime) {
+      return res.status(400).json({ error: "No flight arrival time found" });
+    }
+
+    const flightArrivalTimeStr = String(flightArrivalTime);
+    if (!isISOStringDate(flightArrivalTimeStr)) {
+      return res
+        .status(400)
+        .json({ error: "The flight arrival time is not ISO" });
+    }
+    const recommendation =
+      await buildPickUpRecommendation(flightArrivalTimeStr);
     res.json(recommendation);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch leave time" });
+    res.status(500).json({ error: "Failed to get pick up recommendation" });
   }
+};
+
+export const isISOStringDate = (isoString: string): boolean => {
+  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(isoString)) {
+    return false;
+  }
+  return true;
 };
--- server/src/services/pickUpRecommendationService.ts
+++ server/src/services/pickUpRecommendationService.ts
@@ -1,3 +1,33 @@
-export const buildPickUpRecommendation = async () => {
-  return "leave now";
+import PickUpRecommendation from "../models/PickUpRecommendation";
+
+const MINUTES_WALKING_FROM_AIRPORT_TO_PARKING = 20;
+
+export const buildPickUpRecommendation = async (
+  arrivalTime: string
+): Promise<PickUpRecommendation> => {
+  const datePassengerIsAtParking = addMinutes(
+    MINUTES_WALKING_FROM_AIRPORT_TO_PARKING,
+    arrivalTime
+  );
+  if (datePassengerIsAtParking <= new Date()) return { leaveAt: "now" };
+
+  const travelTimeFromLocationToAirport = 40; // this will come from the google api
+  const dateAtTheAirportLeavingNow = addMinutes(
+    travelTimeFromLocationToAirport
+  );
+  if (datePassengerIsAtParking <= dateAtTheAirportLeavingNow)
+    return { leaveAt: "now" };
+
+  const leaveLocationAt = addMinutes(
+    -travelTimeFromLocationToAirport,
+    datePassengerIsAtParking.toISOString()
+  ).toISOString();
+
+  return { leaveAt: leaveLocationAt };
 };
+
+function addMinutes(minutes: number, date?: string): Date {
+  let result = new Date();
+  if (date) result = new Date(date);
+  return new Date(result.setMinutes(result.getMinutes() + minutes));
+}
