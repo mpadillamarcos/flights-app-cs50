bdc34113ac9139a0c4d0572dc91be9b4b21b291a
Maria Padilla <maria.pad.mar@gmail.com>
2025-12-23 11:30:58 +0100

    feat: vue project skeleton cleanup

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import FlightSearchBars from "../components/FlightSearchBars.vue"
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <FlightSearchBars />
   </main>
 </template>
