40d52042536871dc1edf0e489880a2bcb42fdd64
Maria Padilla <maria.pad.mar@gmail.com>
2025-12-23 15:37:06 +0100

    feat: add flight origin selector

--- client/src/components/FlightSearchBars.vue
+++ client/src/components/FlightSearchBars.vue
@@ -3,11 +3,19 @@ import { ref, watch } from "vue"
 import { useRoute, useRouter } from "vue-router";
 import SearchIcon from "../components/icons/SearchIcon.vue"
 
+defineProps({
+    flightOrigins: {
+        type: Array<string>,
+        default: () => []
+    }
+})
+
 const route = useRoute();
 const router = useRouter();
 
 const selectedSearchOption = ref("flightNumber");
 const flightNumber = ref(null);
+const flightOrigin = ref(null);
 
 const handleSearchByFlightNumber = () => {
     router.replace({
@@ -18,12 +26,23 @@ const handleSearchByFlightNumber = () => {
     });
 }
 
+const handleSearchByFlightOrigin = () => {
+    router.replace({
+        query: {
+            ...route.query,
+            flightOrigin: flightOrigin.value,
+        },
+    });
+}
+
 watch(selectedSearchOption, (newSelectedOption) => {
     router.replace({
         query: {
             searchOption: newSelectedOption,
         },
     });
+    flightNumber.value = null
+    flightOrigin.value = null
 });
 </script>
 
@@ -39,6 +58,16 @@ watch(selectedSearchOption, (newSelectedOption) => {
             <div class="row">
                 <input v-model="flightNumber" placeholder="Enter flight number" />
                 <button class="search button" @click="handleSearchByFlightNumber">
+                    <SearchIcon />
+                </button>
+            </div>
+        </div>
+        <div v-else>
+            <div class="row">
+                <select v-model="flightOrigin">
+                    <option v-for="origin in flightOrigins" :id="origin">{{ origin }}</option>
+                </select>
+                <button class="search button" @click="handleSearchByFlightOrigin">
                     <SearchIcon />
                 </button>
             </div>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import FlightSearchBars from "../components/FlightSearchBars.vue"
 import api from "../api/api"
 
+const flights = ref(null)
+
+const flightOrigins = computed(() => {
+  if (!flights.value) return [];
+  return Object.keys(flights.value).map(origin => origin).sort((a, b) => a.localeCompare(b))
+})
+
 onMounted(async () => {
-  const flights = await api.getFlights()
-  console.log(flights)
+  flights.value = await api.getFlights()
 })
 </script>
 
 <template>
   <main>
-    <FlightSearchBars />
+    <FlightSearchBars :flight-origins="flightOrigins" />
   </main>
 </template>
