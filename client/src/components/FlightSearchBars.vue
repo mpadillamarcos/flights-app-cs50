03d3fb79e8b3aff28dd5a6e15c4a876dca87bc49
Maria Padilla <maria.pad.mar@gmail.com>
2025-12-23 13:23:05 +0100

    feat: add search options toggle

--- client/src/components/FlightSearchBars.vue
+++ client/src/components/FlightSearchBars.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue"
+
+const selectedSearchOption = ref("flightNumber");
+</script>
+
 <template>
     <div>
-        Search bars
+        <div id="toggle">
+            <button class="button" :class="{ 'button-outline': selectedSearchOption != 'flightNumber' }"
+                @click="selectedSearchOption = 'flightNumber'">Flight number</button>
+            <button class="button" :class="{ 'button-outline': selectedSearchOption != 'flightOrigin' }"
+                @click="selectedSearchOption = 'flightOrigin'">Flight origin</button>
+        </div>
     </div>
 </template>
+
+<style>
+.button:focus {
+    background-color: #9b4dca;
+    border-color: #9b4dca;
+}
+</style>
