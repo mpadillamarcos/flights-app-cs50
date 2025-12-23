5be6ae5649bb7ff110eb4f3733372a5d311702b9
Maria Padilla <maria.pad.mar@gmail.com>
2025-12-23 15:17:04 +0100

    feat: add search bar

--- client/src/components/FlightSearchBars.vue
+++ client/src/components/FlightSearchBars.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { ref, watch } from "vue"
 import { useRoute, useRouter } from "vue-router";
+import SearchIcon from "../components/icons/SearchIcon.vue"
 
 const route = useRoute();
 const router = useRouter();
 
 const selectedSearchOption = ref("flightNumber");
+const flightNumber = ref(null);
+
+const handleSearchByFlightNumber = () => {
+    router.replace({
+        query: {
+            ...route.query,
+            flightNum: flightNumber.value,
+        },
+    });
+}
 
 watch(selectedSearchOption, (newSelectedOption) => {
     router.replace({
         query: {
-            ...route.query,
             searchOption: newSelectedOption,
         },
     });
@@ -25,6 +35,14 @@ watch(selectedSearchOption, (newSelectedOption) => {
             <button class="button" :class="{ 'button-outline': selectedSearchOption != 'flightOrigin' }"
                 @click="selectedSearchOption = 'flightOrigin'">Flight origin</button>
         </div>
+        <div v-if="selectedSearchOption === 'flightNumber'" id="searchBar">
+            <div class="row">
+                <input v-model="flightNumber" placeholder="Enter flight number" />
+                <button class="search button" @click="handleSearchByFlightNumber">
+                    <SearchIcon />
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -32,5 +50,14 @@ watch(selectedSearchOption, (newSelectedOption) => {
 .button:focus {
     background-color: #9b4dca;
     border-color: #9b4dca;
+}
+
+.search {
+    display: flex;
+    align-items: center;
+}
+
+.search:hover {
+    background-color: #606C76;
 }
 </style>
